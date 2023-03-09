7ec1da78d5b252bcfcb28f0bee4d01920c42f6c6
hafiyb <hafiyb@gmail.com>
2023-03-09 21:50:50 +0800

    feat: search function, SEO, SSG, etc

--- src/pages/contact/[id].tsx
+++ src/pages/contact/[id].tsx
@@ -1,6 +1,9 @@
 import EpisodeCard from '@/components/episodeCard';
 import PersonalInfoLine from '@/components/personalInfoLine';
-import { Box, Typography } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { Box, IconButton, Typography } from '@mui/material';
+import Head from 'next/head';
+import Link from 'next/link';
 
 const styles = {
   container: {
@@ -12,7 +15,7 @@ const styles = {
     backgroundColor: 'secondary.main',
     px: {
       xs: '5%',
-      sm: '20%'
+      sm: '20%',
     },
     py: 3,
     m: 4,
@@ -36,43 +39,68 @@ interface Prop {
 const CharacterPage = (prop: Prop) => {
   let character = prop.character;
   let episodes = Array.isArray(prop.episodes) ? prop.episodes : [prop.episodes];
+  let metaDescription = `View information about ${character.name}`
+
   return (
-    <Box sx={styles.container}>
-      <Box sx={styles.headerContainer}>
-        <Box
-          component='img'
-          src={character.image}
-          sx={{
-            width: '200px',
-            height: '200px',
-            borderRadius: '50%',
-            border: '5px solid',
-            borderColor: 'primary.main',
-          }}
+    <>
+      <Head>
+        <title>{character.name} - SleekFlow</title>
+        <meta
+          name='description'
+          content={metaDescription}
         />
-        <Typography sx={{ width: '100%', textAlign: 'center' }}>
-          {character.name}
-        </Typography>
-      </Box>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Box sx={styles.container}>
+        <Link href='/'>
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+        </Link>
+        <Box sx={styles.headerContainer}>
+          <Box
+            component='img'
+            src={character.image}
+            sx={{
+              width: '200px',
+              height: '200px',
+              borderRadius: '50%',
+              border: '5px solid',
+              borderColor: 'primary.main',
+            }}
+          />
+          <Typography
+            sx={{
+              width: '100%',
+              textAlign: 'center',
+              fontSize: '28px',
+              color: 'black',
+            }}
+          >
+            {character.name}
+          </Typography>
+        </Box>
 
-      <Box sx={styles.sectionContainer}>
-        <Typography>Personal info</Typography>
-        <PersonalInfoLine title={'Status'} value={character.status} />
-        <PersonalInfoLine title={'Gender'} value={character.gender} />
-        <PersonalInfoLine title={'Location'} value={character.location} />
-        <PersonalInfoLine title={'Origin'} value={character.origin} />
-        <PersonalInfoLine title={'Species'} value={character.species} />
-      </Box>
+        <Box sx={styles.sectionContainer}>
+          <Typography>Personal info</Typography>
+          <PersonalInfoLine title={'Status'} value={character.status} />
+          <PersonalInfoLine title={'Gender'} value={character.gender} />
+          <PersonalInfoLine title={'Location'} value={character.location} />
+          <PersonalInfoLine title={'Origin'} value={character.origin} />
+          <PersonalInfoLine title={'Species'} value={character.species} />
+        </Box>
 
-      <Box sx={styles.sectionContainer}>
-        <Typography>Episodes</Typography>
-        <Box sx={{ width: '100%', maxHeight: '500px', overflow: 'auto' }}>
-          {episodes.map((ep: any) => (
-            <EpisodeCard episode={ep} />
-          ))}
+        <Box sx={styles.sectionContainer}>
+          <Typography>Episodes</Typography>
+          <Box sx={{ width: '100%', maxHeight: '500px', overflow: 'auto' }}>
+            {episodes.map((ep: any) => (
+              <EpisodeCard episode={ep} />
+            ))}
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
@@ -80,15 +108,11 @@ export const getStaticPaths = async () => {
   const res = await fetch('https://rickandmortyapi.com/api/character');
   const posts = await res.json();
 
-  // const paths = posts.results.map((post: any) => ({
-  //   params: { id: post.id.toString() },
-  // }));
-
-  const paths = []
-  for(let i = 1; i <= posts.info.pages; i++){
+  const paths = [];
+  for (let i = 1; i <= posts.info.count; i++) {
     paths.push({
-      params: { id: i.toString()}
-    })
+      params: { id: i.toString() },
+    });
   }
 
   return { paths, fallback: false };
